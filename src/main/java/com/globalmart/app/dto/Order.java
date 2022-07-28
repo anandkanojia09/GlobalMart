6b112a6d1f555ea5729da7e1669a9f8abfff90f5
abhiydvrao <78404237+abhiydvrao@users.noreply.github.com>
2022-07-28 14:12:05 +0530

    order changes

--- src/main/java/com/globalmart/app/dto/Order.java
+++ src/main/java/com/globalmart/app/dto/Order.java
@@ -1,5 +1,60 @@
 package com.globalmart.app.dto;
 
-public class Order {
+import javax.persistence.Entity;
+import javax.persistence.Id;
 
+@Entity
+public class Order {
+	@Id
+     private long orderId;
+	 private double amount;
+	 private String billingDate;
+	 private String customer;
+	 private String paymentMethod;
+	 
+	public Order() {
+		super();
+		this.orderId=orderId;
+		this.amount=amount;
+		this.billingDate=billingDate;
+		this.customer=customer;
+		this.paymentMethod=paymentMethod;
+		
+		// TODO Auto-generated constructor stub
+	}
+	public long getOrderId() {
+		return orderId;
+	}
+	public void setOrderId(long orderId) {
+		this.orderId = orderId;
+	}
+	public double getAmount() {
+		return amount;
+	}
+	public void setAmount(double amount) {
+		this.amount = amount;
+	}
+	public String getBillingDate() {
+		return billingDate;
+	}
+	public void setBillingDate(String billingDate) {
+		this.billingDate = billingDate;
+	}
+	public String getCustomer() {
+		return customer;
+	}
+	public void setCustomer(String customer) {
+		this.customer = customer;
+	}
+	public String getPaymentMethod() {
+		return paymentMethod;
+	}
+	public void setPaymentMethod(String paymentMethod) {
+		this.paymentMethod = paymentMethod;
+	}
+	 
+
+	
+
+	
 }
