6b10f56f5c81f22678ff520cccfd80db27fd03f4
Anant Patel <110079898+anantpatel31@users.noreply.github.com>
2022-08-15 21:21:52 +0530

    refactored code

--- src/main/java/com/globalmart/app/services/ProductServiceImpl.java
+++ src/main/java/com/globalmart/app/services/ProductServiceImpl.java
@@ -1,8 +1,6 @@
 package com.globalmart.app.services;
-
 import java.util.List;
 import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
